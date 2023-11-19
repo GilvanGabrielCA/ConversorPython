d2e67c705e93c1d7b1bb42a6df86f4d8332f8f35
Gilvan Gabriel <gilvangabriealencar@gmail.com>
2023-11-19 15:50:00 -0400

    Corrigido problema de loop eterno em uma so alternativa

--- Conversor.py
+++ Conversor.py
@@ -5,7 +5,7 @@ print("******************")
 print("Bem vindo ao conversor geral Python")
 
 print("O que deseja converter?")
-print("(1) - Medidas * (2) - Temperatura * (3) - Moedas")
+print("(1) - Medidas * (2) - Temperatura * (3) - Saindo")
 
 op = int(input("Escolha: "))
 
@@ -22,24 +22,25 @@ while 0 < op < 4:
             if tp == 1:
                 km = float(input("Informe a kilometragem: "))
                 m = km * 1000
-
                 print("{} kilometros correspondem a {} metros".format(km, m))
+                break
 
             elif tp == 2:
                 m = float(input("Informe a metragem: "))
                 km = m / 1000
-
                 print("{} Metros correspondem a {} kilometros".format(m, km))
+                break
 
             elif tp == 3:
                 km = float(input("Informe a kilometragem: "))
                 milhas = km / 1.609 #para um resultado aproximado, o valor é divido por 1,609
-
                 print("{} kilometros correspondem a {} milhas".format(km, milhas))
+                break
 
             elif tp == 4:
                 milhas = float(input("Informe a milhas: "))
                 km = milhas * 1.609 #para um resultado aproximado, multiplique o valor de comprimento por 1,609
+                break
 
     elif op == 2:
         print("*CONVERSOR DE TEMPERATURA*")
@@ -54,35 +55,52 @@ while 0 < op < 4:
             if esc == 1:
                 c = float(input("Informe a temperatura em celsius: "))
                 f = (c * 9 / 5) + 32
-
                 print("{}°C = {}°F".format(c, f))
+                break
 
             elif esc == 2:
                 c = float(input("Informe a temperatura em celsius: "))
-                k = c + 273,15
-
+                k = c + 273.15
                 print("{}°C = {}°K".format(c, k))
+                break
 
             elif esc == 3:
                 f = float(input("Informe a temperatura em fahrenheit: "))
                 c = (f - 32) * 5 / 9
-
                 print("{}°F = {}°C".format(f, c))
+                break
 
             elif esc == 4:
                 f = float(input("Informe a temperatura em fahrenheit: "))
                 k = (f - 32) * 5 / 9 + 273.15
-
                 print("{}°F = {}°K".format(f, k))
+                break
 
             elif esc == 5:
                 k = float(input("Informe a temperatura em Kelvin: "))
                 c = k - 273.15
-
                 print("{}°K = {}°C".format(k, c))
+                break
 
             elif esc == 6:
                 k = float(input("Informe a temperatura em Kelvin: "))
                 f = (k - 273.15) * 9/5 + 32
-
                 print("{}°K = {}°C".format(k, f))
+                break
+
+    elif op == 3:
+        print("Saindo...")
+        op += 1
+
+    if op != 4:
+        repete = int(input("Deseja continuar? [S/N]: ").upper())
+
+        if repete == "N":
+            print("Saindo...")
+            op += 1
+
+        else:
+            print("O que deseja converter?")
+            print("(1) - Medidas * (2) - Temperatura * (3) - Moedas")
+
+            op = int(input("Escolha: "))
